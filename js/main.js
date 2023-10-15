9ee3235e8fe27dc45c45d453a7de288fcb11f914
ALPINISTLAR <niveam424@gmail.com>
2023-10-15 22:45:39 +0200

    add commit for examiner

---
+++ js/main.js
@@ -0,0 +1,3 @@
+setTimeout(function () {
+  alert('Assalomu aleykum, hozircha faqat Desktop va Tablet dizanyga ulgura oldim. Vaqt bo`lishi bilan mobile dizaynni ham qo`shib qo`yaman! :-)')
+}, 3500);
